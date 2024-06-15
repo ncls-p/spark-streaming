aea34c65a53e5f14d72d57cc348cd38eb0d79774
Nicolas Pierrot <nicolas@archipels.io>
2024-06-15 14:07:01 +0200

    feat: add basic analysis

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 package org.example.spark
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.SparkConf
 import scalaj.http.Http
 import org.json4s.DefaultFormats
@@ -21,106 +21,36 @@ object MainClass extends SparkSessionTrait {
 
     val task = new Runnable {
       def run(): Unit = {
-        try {
-          val response = Http("https://api.sncf.com/v1/coverage/sncf/lines?count=2000")
-            .header("Content-Type", "application/json")
-            .header("Charset", "UTF-8")
-            .header("Authorization", "89c6d939-9245-494f-8b5a-1522fa890871")
-            .asString
-            .body
+        // Download JSON data and save to file
+        downloadJson("https://api.sncf.com/v1/coverage/sncf/lines?count=2000", "sncf_lines")
+        downloadJson("https://api.sncf.com/v1/coverage/sncf/", "sncf_path")
+        downloadJson("https://api.sncf.com/v1/coverage/sncf/lines/line:SNCF:A/stop_points", "sncf_stop_point_rer_a")
+        downloadJson("https://api.sncf.com/v1/coverage/sncf/stop_areas/", "sncf_stop_areas")
 
-          val currentDate = java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm"))
-          val jsonFilePath = s"./data/sncf_lines-$currentDate.json"
+        // Create Spark session
+        val spark = SparkSession.builder().appName("JSON to DataFrame").getOrCreate()
+        import spark.implicits._
 
-          val dirPath: Path = Paths.get("./data")
-          if (!Files.exists(dirPath)) {
-            Files.createDirectory(dirPath)
-            Files.setPosixFilePermissions(dirPath, PosixFilePermissions.fromString("rwxrwxrwx"))
-          }
+        // Read JSON files into DataFrames
+        val dfLines = readJson(spark, "./data/sncf_lines-*.json")
+        val dfPath = readJson(spark, "./data/sncf_path-*.json")
+        val dfStopPoints = readJson(spark, "./data/sncf_stop_point_rer_a-*.json")
+        val dfStopAreas = readJson(spark, "./data/sncf_stop_areas-*.json")
 
-          Files.write(Paths.get(jsonFilePath), response.getBytes(StandardCharsets.UTF_8))
+        // Display DataFrames
+        dfLines.show()
+        dfPath.show()
+        dfStopPoints.show()
+        dfStopAreas.show()
 
-          println(s"Data saved to: $jsonFilePath")
+        // Basic analysis on DataFrames
+        println(s"Total lines: ${dfLines.count()}")
+        println(s"Total stop points: ${dfStopPoints.count()}")
+        println(s"Total stop areas: ${dfStopAreas.count()}")
 
-        } catch {
-          case e: Exception =>
-            e.printStackTrace()
-        }
-        try {
-          val response = Http("https://api.sncf.com/v1/coverage/sncf/")
-            .header("Content-Type", "application/json")
-            .header("Charset", "UTF-8")
-            .header("Authorization", "89c6d939-9245-494f-8b5a-1522fa890871")
-            .asString
-            .body
-
-          val currentDate = java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm"))
-          val jsonFilePath = s"./data/sncf_path-$currentDate.json"
-
-          val dirPath: Path = Paths.get("./data")
-          if (!Files.exists(dirPath)) {
-            Files.createDirectory(dirPath)
-            Files.setPosixFilePermissions(dirPath, PosixFilePermissions.fromString("rwxrwxrwx"))
-          }
-
-          Files.write(Paths.get(jsonFilePath), response.getBytes(StandardCharsets.UTF_8))
-
-          println(s"Data saved to: $jsonFilePath")
-
-        } catch {
-          case e: Exception =>
-            e.printStackTrace()
-        }
-      try {
-          val response = Http("https://api.sncf.com/v1/coverage/sncf/lines/line:SNCF:A/stop_points")
-            .header("Content-Type", "application/json")
-            .header("Charset", "UTF-8")
-            .header("Authorization", "89c6d939-9245-494f-8b5a-1522fa890871")
-            .asString
-            .body
-
-          val currentDate = java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm"))
-          val jsonFilePath = s"./data/sncf_stop_point_rer_a-$currentDate.json"
-
-          val dirPath: Path = Paths.get("./data")
-          if (!Files.exists(dirPath)) {
-            Files.createDirectory(dirPath)
-            Files.setPosixFilePermissions(dirPath, PosixFilePermissions.fromString("rwxrwxrwx"))
-          }
-
-          Files.write(Paths.get(jsonFilePath), response.getBytes(StandardCharsets.UTF_8))
-
-          println(s"Data saved to: $jsonFilePath")
-
-        } catch {
-          case e: Exception =>
-            e.printStackTrace()
-        }
-        try {
-          val response = Http("https://api.sncf.com/v1/coverage/sncf/stop_areas/")
-            .header("Content-Type", "application/json")
-            .header("Charset", "UTF-8")
-            .header("Authorization", "89c6d939-9245-494f-8b5a-1522fa890871")
-            .asString
-            .body
-
-          val currentDate = java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm"))
-          val jsonFilePath = s"./data/sncf_stop_areas-$currentDate.json"
-
-          val dirPath: Path = Paths.get("./data")
-          if (!Files.exists(dirPath)) {
-            Files.createDirectory(dirPath)
-            Files.setPosixFilePermissions(dirPath, PosixFilePermissions.fromString("rwxrwxrwx"))
-          }
-
-          Files.write(Paths.get(jsonFilePath), response.getBytes(StandardCharsets.UTF_8))
-
-          println(s"Data saved to: $jsonFilePath")
-
-        } catch {
-          case e: Exception =>
-            e.printStackTrace()
-        }
+        dfLines.printSchema()
+        dfStopPoints.printSchema()
+        dfStopAreas.printSchema()
       }
     }
 
@@ -134,5 +64,36 @@ object MainClass extends SparkSessionTrait {
     if (!scheduler.awaitTermination(1, TimeUnit.MINUTES)) {
       scheduler.shutdownNow()
     }
+  }
+
+  def downloadJson(url: String, filenamePrefix: String): Unit = {
+    try {
+      val response = Http(url)
+        .header("Content-Type", "application/json")
+        .header("Charset", "UTF-8")
+        .header("Authorization", "89c6d939-9245-494f-8b5a-1522fa890871")
+        .asString
+        .body
+
+      val currentDate = java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm"))
+      val jsonFilePath = s"./data/$filenamePrefix-$currentDate.json"
+
+      val dirPath: Path = Paths.get("./data")
+      if (!Files.exists(dirPath)) {
+        Files.createDirectory(dirPath)
+        Files.setPosixFilePermissions(dirPath, PosixFilePermissions.fromString("rwxrwxrwx"))
+      }
+
+      Files.write(Paths.get(jsonFilePath), response.getBytes(StandardCharsets.UTF_8))
+
+      println(s"Data saved to: $jsonFilePath")
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+  }
+
+  def readJson(spark: SparkSession, path: String): DataFrame = {
+    spark.read.json(path)
   }
 }
